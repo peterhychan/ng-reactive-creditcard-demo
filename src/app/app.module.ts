11343e78074d7b1e27a6b045b9e3ede02eb2ac31
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-01-28 15:33:16 -0800

    app completed and deployed on now.sh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { CardFormComponent } from "./card-form/card-form.component";
+import { InputComponent } from "./input/input.component";
+import { CardComponent } from "./card/card.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFormComponent,
+    InputComponent,
+    CardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
